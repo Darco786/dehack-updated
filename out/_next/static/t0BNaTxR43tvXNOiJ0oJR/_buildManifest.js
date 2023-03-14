337331c383f875d545d3f748d9c0f624374186f2
Medwebdev <mtakisnani09@gmail.com>
2023-03-14 12:49:51 +0100

    ios problem fixing

---
+++ out/_next/static/t0BNaTxR43tvXNOiJ0oJR/_buildManifest.js
@@ -0,0 +1,12 @@
+;(self.__BUILD_MANIFEST = {
+  __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+  '/': [
+    'static/chunks/ea88be26-3cdde98635804176.js',
+    'static/css/33cdbccfa26f416a.css',
+    'static/chunks/747-625cbdfe991dc7b7.js',
+    'static/chunks/pages/index-fa7a2f0600c6af5a.js',
+  ],
+  '/_error': ['static/chunks/pages/_error-e4f561a102d9bb14.js'],
+  sortedPages: ['/', '/_app', '/_error'],
+}),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
